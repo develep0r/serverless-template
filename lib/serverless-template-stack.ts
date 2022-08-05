d95225a0866660288067d09c32fdc9d19be822aa
Siddartha Polisetty <siddarthapolisetty@gmail.com>
2022-08-04 21:33:59 -0700

    code pipeline staging and production pass#1

--- lib/serverless-template-stack.ts
+++ lib/serverless-template-stack.ts
@@ -1,8 +1,9 @@
-import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps, Stage } from 'aws-cdk-lib';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunctionProps, NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { join } from 'path';
 
@@ -58,5 +59,25 @@ export class ServerlessTemplateStack extends Stack {
     
     const singleEmployee = employee.addResource('{id}'); 
     singleEmployee.addMethod('GET'); 
+
+  // Code Pipeline Setup
+  const codePipeline = new CodePipeline(this, 'Employee Pipeline', {
+    pipelineName: 'Employee Pipeline',
+    synth: new ShellStep('Build', {
+      input: CodePipelineSource.gitHub('develep0r/serverless-template', 'master'),
+      commands: ['npm ci', 'npm run build', 'npx cdk synth']
+    })
+  });
+
+  const staging = codePipeline.addStage(new Stage(this, 'staging', {
+    env: { account: '478602235759', region: 'us-east-1'}
+  }));
+
+  staging.addPost(new ManualApprovalStep('APPROVAL REQUIRED: Deploy to Production'));
+
+  const production = codePipeline.addStage(new Stage(this, 'production', {
+    env: { account: '478602235759', region: 'us-east-1'}
+  }));
+
   }
 }
